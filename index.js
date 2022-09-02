a3be052d4f758cb10ad0a78443d3f6bb413bc02d
Kumar Sumit <kumarsumit1989@gmail.com>
2022-09-02 19:42:36 +0530

    Set up Ejs and views folder in root/index.js file and installed ejs

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const port = 8080;
 app.use('/', require('./routes'));
 // this by default will fetch routes/index.js files in index.js
+app.set('view engine', 'ejs');
+//this will set ejs as the view engine
+app.set('views', './views');
+// this will set views to look for views in the views folder
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running the server: ${err}`)
