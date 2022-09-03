06b45144af7bb02a7967de35b34304339f4e1df9
Kumar Sumit <kumarsumit1989@gmail.com>
2022-09-03 13:52:45 +0530

    Create two controllers in users controllers for sign in and sign up form submissions

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,13 +1,17 @@
 module.exports.profile = function(req, res){
     res.send('<h1>User Profile</h1>')
 }
-
+//Controller for Sign Up Form Submission
+module.exports.create = function(req, res){}
+//Controller for Sign In Form Submission
+module.exports.createSession = function(req, res){}
 //Controller to render the signin page
 module.exports.signUp = function(req, res){
     return res.render('user_sign_up', {
         title: "FriendBook | Sign Up"
     })
 }
+
 
 
 //controller to render the signup page
