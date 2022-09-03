b46f5e2d9e0a2280ee3886e0ca74640b395af6ea
Kumar Sumit <kumarsumit1989@gmail.com>
2022-09-03 14:24:45 +0530

    Complete the controller for sign up form submission

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,4 @@
-const { notDeepEqual } = require("assert")
+const User = require('../models/users');
 
 module.exports.profile = function(req, res){
     res.send('<h1>User Profile</h1>')
@@ -6,8 +6,33 @@ module.exports.profile = function(req, res){
 //Controller for Sign Up Form Submission
 // create User
 module.exports.create = function(req, res){
-    // first check whether password and confirm password are equal or notEqual, if they are not equal send the user back to the sign up page
     console.log(req.body);
+    // first check whether password and confirm password are equal or notEqual, if they are not equal send the user back to the sign up page
+    if(req.body.password !== req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email:req.body.email}, function(err, user){
+        if(err){
+            console.log('error in signing up');
+            return;
+        }
+        if(!user){
+            //means no previous user is associated with that email, create and store the new user
+            User.create(req.body, function(err, user){
+                if(err){
+                    console.log('error in creating user while signing up');
+                    return;
+                }
+                //after creating the user redirect the user to sign in page for the new user to sign in
+                return res.redirect('users/sign-in');
+            })
+        }
+        // if the user is already present send the control back to sign up page
+        else{
+            return res.redirect('back');
+        }
+    })
+
 }
 //Controller for Sign In Form Submission
 module.exports.createSession = function(req, res){}
