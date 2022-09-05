7433a082bf6cb0a1b1773cd47216f85057aafd5c
Kumar Sumit <kumarsumit1989@gmail.com>
2022-09-05 20:00:56 +0530

    Display Comments for each post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,7 +2,15 @@ const Post = require('../models/posts');
 //populate the user of each post
 module.exports.home = function(req, res)
 {   
-    Post.find({}).populate('user').exec(function(err, posts)
+    Post.find({})
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts)
     {
         if (err) return handleError(err);
         return res.render('home', {
