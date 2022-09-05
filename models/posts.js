a5dc8ac325945bd72ae052f348a9656b97c9d4da
Kumar Sumit <kumarsumit1989@gmail.com>
2022-09-05 09:45:36 +0530

    Create a post schema to create posts and link the post to user Schema in models folder

---
+++ models/posts.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const postSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    user: {
+        type:  mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+        // here we are referecing the user schema
+    }
+},{
+    timestamps: true
+    // we have used timestamps to store createdAt and updatedAt
+});
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
