c97f159ff7dd04b9c435894252ec7d66bd95f49c
Kumar Sumit <kumarsumit1989@gmail.com>
2022-09-05 20:16:39 +0530

    Delete a specific Post and associated Comments

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/posts');
+const Comment = require('../models/comments');
 module.exports.create = function (req, res) {
     Post.create({
         content: req.body.content,
@@ -10,4 +11,23 @@ module.exports.create = function (req, res) {
         }
         return res.redirect('back');
     })
+}
+
+//delete a post and all associated comments
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+        // .id means converting the object id into string
+        if (post.user == req.user.id){
+            post.remove();
+            //go inside comments and search all the comments belonging to a particular post and delete them
+            Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect('back');
+            });
+        }
+        //if no post is found for that id, send the control back
+        else{
+            return res.redirect('back');
+        }
+
+    });
 }
--- routes/posts.js
+++ routes/posts.js
@@ -4,4 +4,5 @@ const passport = require('passport');
 //require the postsController to link the actions with the routes
 const postsController = require('../controllers/posts_controller');
 router.post('/create', passport.checkAuthentication,postsController.create);
+router.get('/destroy/:id', passport.checkAuthentication,postsController.destroy);
 module.exports = router;
