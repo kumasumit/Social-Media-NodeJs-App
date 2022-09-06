9e69d76fe4d95ed1e15679cf7617ddca2eb1f9e9
Kumar Sumit <kumarsumit1989@gmail.com>
2022-09-06 17:59:21 +0530

    Display all the users and also a friends section

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,22 +1,33 @@
 const Post = require('../models/posts');
+const User = require('../models/users');
 //populate the user of each post
 module.exports.home = function(req, res)
 {   
-    Post.find({})
+    Post.find({}) 
+    //find({}) this will find all the posts
     .populate('user')
+    //this will popluate the user which created that post
     .populate({
         path:'comments',
+        // this will populate all the comments for that post
         populate: {
             path: 'user'
+        // this will populate the user which created that comment    
         }
     })
     .exec(function(err, posts)
-    {
+    {   
         if (err) return handleError(err);
-        return res.render('home', {
-            title:"Home",
-            posts: posts
-        });    
+        User.find({}, function(err, users){
+        //User.find({}) will display all the users in the database    
+            return res.render('home', {
+                title:" FreindBook || Home",
+                posts: posts,
+                all_users: users
+            });    
+        })
+        
+        
     })     
 }
 
