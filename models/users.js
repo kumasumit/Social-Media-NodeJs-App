ad5c7a0dba4ca4e6abfc4da1fff39d961b528e78
Kumar Sumit <kumarsumit1989@gmail.com>
2022-09-10 14:53:38 +0530

    Modify the userSchema to have avatar image link as a string

--- models/users.js
+++ models/users.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/upload/users/avatars')
+//'/upload/users/avatars' is the path where we store our avaatars locally
 const userSchema = new mongoose.Schema({
     email:{
         type: String,
@@ -11,7 +15,11 @@ const userSchema = new mongoose.Schema({
     }, name: {
         type: String,
         required: true
+    },
+    avatar: {
+        type:String
     }
+    // avatar will store the link to where the avatar image is stored in form of a link
 },{
     timestamps: true
 //   timestamps store the value createdAt and updatedAt
